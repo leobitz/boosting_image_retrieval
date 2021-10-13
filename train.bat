e032daa63c7a6c5f1d33a8170e4285fb8524c10e
Amanuel Negash <amanyamy@gmail.com>
2021-10-13 11:04:23 +0300

    train start from last

--- train.bat
+++ train.bat
@@ -1,13 +1,18 @@
 @ECHO OFF
-for %%v in (resnet50 resnet101) do ( REM pca resent18 resent50 resent101 vit
-	for %%l in (0.001 0.0001) do (
+for %%v in (vit) do ( @REM pca resent18 resent50 resent101 vit
+	for %%l in (0.001) do (
 		for %%t in (single double) do (
             for %%r in (1 2 3 4 5) do (
-                if exist result\pos\%%v-%%l-%%t-%%r (
+                if exist result\trains\%%t-%%v-%%l-%%r (
                     echo file exists
                 ) else (
-                    echo result\pos\%%v-%%l-%%t-%%r 
+                    if exist result\saves\%%t-%%v-%%l-%%r (
+                        echo folder exists
+                        @RD /S /Q "result\saves\%%t-%%v-%%l-%%r"
+                    ) 
+                    echo result\pos\%%t-%%v-%%l-%%r
                     python .\train.py --model_type %%t --vec_type %%v --learning_rate %%l --run %%r 
+                    copy NUL result\trains\%%t-%%v-%%l-%%r
                 )
 			)
 		)
