0e4431bb33d1850e4c17afd0a5b9eb69640b453d
Amanuel Negash <amanyamy@gmail.com>
2021-11-05 17:06:30 +0300

    eval fix

--- proc_lib.py
+++ proc_lib.py
@@ -100,19 +100,39 @@ def display_similars(name, sim_mat):
     plt.show()
 
 
-def evaluate(ground, pred):
-    pres = []
-    found = 0
-    for pi in range(len(ground)):
-        try:
-            gindex = ground.index(pred[pi])
-            found += 1
-            p = found / (gindex + 1)
-            pres.append(p)
-        except:
-            pass
-    return np.sum(pres) / len(ground)
+def evaluate(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
 
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
 
 def get_acc(psim, test, map2idx, idx2map, sim_mat):
     accs = []
